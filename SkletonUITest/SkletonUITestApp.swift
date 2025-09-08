9644ee5879356b870093006fb1d7df0fc2d9ca4c
PhungMHieu <kiritosad41@gmail.com>
2025-09-09 06:41:59 +0700

    Initial Commit

---
+++ SkletonUITest/SkletonUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkletonUITestApp.swift
+//  SkletonUITest
+//
+//  Created by Admin on 9/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SkletonUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
